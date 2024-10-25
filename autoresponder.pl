9b64aec668cb5f24adaa268ac898223424bf05ec
Gavin Hayes <gavin@dylibso.com>
2024-10-24 22:10:05 -0400

    feat: inviting guests by emailing them link, loading config from .env

--- autoresponder.pl
+++ autoresponder.pl
@@ -1,87 +1,141 @@
 #!/usr/bin/perl
 use strict; use warnings;
+use Data::Dumper;
 use Extism ':all';
 use JSON::PP qw(decode_json);
-my ($sender, $receiver) = @ARGV;
-my %email = (sender => $sender, receiver => $receiver);
+use XTP;
 
-# parse headers
-my $prevheader;
-while(my $line = <STDIN>) {
-    if ($line eq "\n") {
-        last;
+# load XTP configuration
+my $appid = $ENV{XTP_APPID};
+my $token = $ENV{XTP_TOKEN};
+
+# parse the incoming email
+my %email = parse_incoming_email(@ARGV);
+my $sender = $email{sender};
+my $receiver = $email{receiver};
+
+# if the receiver is root, check if it's a guest signup email
+# if so reply with an invite link
+if ($receiver eq 'root@localhost') {
+    my $subject = exists $email{headers}{Subject} ? $email{headers}{Subject} : '';
+    if ($subject =~ m!^/hackablee?mail\s+signup!) {
+        eval {
+            my $client = XTP::Client->new({
+                token => $token,
+                appId => $appid,
+            });
+            my $invite = $client->inviteGuest({
+                guestKey => email_to_guestKey($sender),
+                deliveryMethod => 'link'
+            });
+            my %email = (
+                headers => {
+                    Subject => 'Hackable Email Invite',
+                    To => $sender,
+                    From => $receiver,
+                },
+                body => $invite->{link}
+            );
+            exit send_email($sender, $receiver, \%email);
+        };
+        if ($@) {
+            exit 70; # EX_SOFTWARE
+        }
     }
-    if ($line =~ /^\s/ && $prevheader) {
-        chomp $line;
-        $email{headers}{$prevheader} .= "\n$line";
-        next;
-    }
-    my ($header, $value) = $line =~ /^([^:]+):(.+)$/ or die "failed to parse header line: '$line'   ";
-    $email{headers}{$header} = $value;
-    $prevheader = $header;
 }
 
-# parse body (only text supported currently)
-my $body = '';
-while(my $line = <STDIN>) {
-    $body .= $line;
+# try to get a plugin
+my $plugin = eval {
+    # Initialize the host functions
+    my $deliver = Extism::Function->new("deliver", [Extism_String], [Extism_String], sub {
+        my ($input) = @_;
+        my $email = decode_json($input);
+        my $rc = send_email($sender, $receiver, $email);
+        return JSON::PP::->new->utf8->allow_nonref->encode($rc);
+    });
+    my $reply = Extism::Function->new("reply", [Extism_String], [Extism_String], sub {
+        my ($input) = @_;
+        my $email = decode_json($input);
+        $email->{headers} = {
+            Subject => $email->{subject} // '',
+            To => $sender,
+            From => $receiver,
+        };
+        my $rc = send_email($sender, $receiver, $email);
+        return JSON::PP::->new->utf8->allow_nonref->encode($rc);
+    });
+
+    # Initialize an XTP client
+    my $client = XTP::Client->new({
+        token => $token,
+        appId => $appid,
+        extism => {functions => [$deliver, $reply], wasi => 1}
+    });
+
+    # Finally actually try to get a plugin
+    $client->getPlugin('on email', email_to_guestKey($receiver));
+};
+if ($@) {
+    # no plugin, or initializing XTP failed, let the email pass through
+    exit send_email($sender, $receiver, \%email);
 }
-$email{body} = $body;
+
+# Call the guest's plugin
+my $rc = eval {
+    my $res = $plugin->call('onEmail', \%email);
+    JSON::PP::->new->utf8->allow_nonref->decode($res)
+};
+if ($@) {
+    # Bounce, the plugin crashed
+    exit 70; # EX_SOFTWARE
+}
+# return the plugin's exit code
+exit $rc;
+
+sub parse_incoming_email {
+    my ($sender, $receiver) = @_;
+    my %email = (sender => $sender, receiver => $receiver);
+    # parse headers
+    my $prevheader;
+    while(my $line = <STDIN>) {
+        if ($line eq "\n") {
+            last;
+        }
+        if ($line =~ /^\s/ && $prevheader) {
+            chomp $line;
+            $email{headers}{$prevheader} .= "\n$line";
+            next;
+        }
+        my ($header, $value) = $line =~ /^([^:]+):\s+(.+)$/ or die "failed to parse header line: '$line'   ";
+        $email{headers}{$header} = $value;
+        $prevheader = $header;
+    }
+
+    # parse body (only text supported currently)
+    my $body = '';
+    while(my $line = <STDIN>) {
+        $body .= $line;
+    }
+    $email{body} = $body;
+    %email
+}
 
 sub send_email {
     my ($sender, $receiver, $email) = @_;
     open(my $chld,  '|-', '/usr/sbin/sendmail', '-i', '-f', $sender, $receiver) // die "failed to open sendmail";
-    open(my $log, '>', '/home/gavin/dev/xtp-email-demo/lastmail.txt');
     while (my ($header, $value) = each(%{$email->{headers}})) {
         my $headerline = "$header: $value\n";
         print $chld $headerline;
-        print $log $headerline; 
     }
-    print "\n";
+    print $chld "\n";
     print $chld $email->{body};
-    print $log $email->{body};
-    # TODO actually return exit code
-    0
+    close($chld);
+    my $fullcode = $?;
+    my $code = $fullcode >> 8;
+    $code
 }
 
-# Host functions
-my $deliver = Extism::Function->new("deliver", [Extism_String], [Extism_String], sub {
-    my ($input) = @_;
-    my $email = decode_json($input);
-    my $rc = send_email($sender, $receiver, $email);
-    return JSON::PP::->new->utf8->allow_nonref->encode($rc);
-});
-
-my $reply = Extism::Function->new("reply", [Extism_String], [Extism_String], sub {
-    my ($input) = @_;
-    my $email = decode_json($input);
-    $email->{headers} = {
-        Subject => $email->{subject} // '',
-        To => $sender,
-        From => $receiver,
-    };
-    my $rc = send_email($sender, $receiver, $email);
-    return JSON::PP::->new->utf8->allow_nonref->encode($rc);
-});
-
-# load the plugin and call it
-my $wasmpath = '/home/gavin/dev/xtp-email-demo/plugin/dist/plugin.wasm';
-my $wasm = do { local(@ARGV, $/) = $wasmpath; <> };
-my ($plugin, $errmsg) = Extism::Plugin->new($wasm, {functions => [$deliver, $reply], wasi => 1});
-$plugin or die $errmsg;
-my ($res, $extismrc, $error) = $plugin->call('onEmail', \%email);
-$res // die "$error rc $extismrc";
-my $rc = JSON::PP::->new->utf8->allow_nonref->decode($res);
-exit $rc;
-
-#my %response = ( headers => {
-#    To => $sender,
-#    From => $receiver,
-#    Subject => 'subject',
-#}, body => 'back atcha');
-#reply(\%response);
-
-# Deliver to inbox
-#send_email($sender, $receiver, \%email);
-
-#exec('/usr/sbin/sendmail', '-i', '-f', $ARGV[0], $ARGV[1]);
+sub email_to_guestKey {
+    my ($address) = @_;
+    unpack('H*', $address)
+}
--- autoresponder.sh
+++ autoresponder.sh
@@ -3,4 +3,7 @@ export PERLBREW_ROOT=/home/gavin/perl5/perlbrew
 export PERLBREW_HOME=/home/gavin/.perlbrew
 source ${PERLBREW_ROOT}/etc/bashrc
 perlbrew use perl-5.40.0
+set -o allexport
+source /home/gavin/dev/xtp-email-demo/.env
+set +o allexport
 exec perl /home/gavin/dev/xtp-email-demo/autoresponder.pl "$@"
